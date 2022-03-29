47d696d725c8a0c5c5337ef94ec43b99c75a6958
Mateusz Qunabu <mateusz@qunabu.com>
2022-03-29 17:25:26 +0200

    Update PdfResource.php

--- src/Http/Resources/PdfResource.php
+++ src/Http/Resources/PdfResource.php
@@ -4,6 +4,8 @@ namespace EscolaLms\TemplatesPdf\Http\Resources;
 
 use EscolaLms\TemplatesPdf\Models\FabricPDF;
 use Illuminate\Http\Resources\Json\JsonResource;
+use EscolaLms\Auth\Models\User;
+
 
 class PdfResource extends JsonResource
 {
@@ -18,6 +20,8 @@ class PdfResource extends JsonResource
             'id' => $this->id,
             'template' => $this->template,
             'path' => $this->path,
+            'user_id' => $this->user_id, 
+            'user' => User::find($this->user_id), 
             'title' => $this->title,
             'content' => $this->content ? json_decode($this->content) : null,
             'created_at' => $this->created_at,
