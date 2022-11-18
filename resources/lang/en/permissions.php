7e0a2462716b7c873b5e82ababb1bc54e58363a3
Maciej Rymarz <59456825+mako321@users.noreply.github.com>
2022-11-18 16:37:01 +0100

    Add translations (#20)

---
+++ resources/lang/en/permissions.php
@@ -0,0 +1,8 @@
+<?php
+
+use EscolaLms\TemplatesPdf\Enums\PdfPermissionsEnum;
+
+return [
+    PdfPermissionsEnum::PDF_READ_ALL => 'Read all pdf',
+    PdfPermissionsEnum::PDF_LIST => 'fabricjs list',
+];
