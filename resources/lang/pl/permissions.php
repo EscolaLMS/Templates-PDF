b6d536690d049bc29b4383d49ed0ffd018ab0a5a
Maciej Rymarz <maciej.rymarz@escolasoft.com>
2022-11-18 16:21:50 +0100

    Add translations

---
+++ resources/lang/pl/permissions.php
@@ -0,0 +1,8 @@
+<?php
+
+use EscolaLms\TemplatesPdf\Enums\PdfPermissionsEnum;
+
+return [
+    PdfPermissionsEnum::PDF_READ_ALL => 'Wyświetl wszystkie pdf',
+    PdfPermissionsEnum::PDF_LIST => 'Lista fabricjs',
+];
--- src/EscolaLmsTemplatesPdfServiceProvider.php
+++ src/EscolaLmsTemplatesPdfServiceProvider.php
@@ -32,6 +32,7 @@ class EscolaLmsTemplatesPdfServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'fabricjs');
 
         if ($this->app->runningInConsole()) {
             $this->bootForConsole();
