4702cf4d39c895e00c3ab49df17900f2ffd9e204
Witold Wiśniewski <58150098+daVitekPL@users.noreply.github.com>
2023-02-07 16:35:44 +0100

    Added course title variable (#24)
    * Added course title variable
    
    * Update composer

--- src/Core/PdfVariables.php
+++ src/Core/PdfVariables.php
@@ -11,12 +11,15 @@ abstract class PdfVariables extends AbstractTemplateVariableClass implements Tem
 {
     const VAR_APP_NAME       = '@VarAppName';
     const VAR_TODAY = '@VarToday';
+    const VAR_COURSE_TITLE = '@VarCourseTitle';
 
     public static function mockedVariables(?User $user = null): array
     {
+        $faker = \Faker\Factory::create();
         return [
             self::VAR_APP_NAME => config('app.name'),
             self::VAR_TODAY => today()->format('d.m.Y'),
+            self::VAR_COURSE_TITLE => $faker->title,
         ];
     }
 
@@ -25,6 +28,7 @@ abstract class PdfVariables extends AbstractTemplateVariableClass implements Tem
         return [
             self::VAR_APP_NAME => config('app.name'),
             self::VAR_TODAY => today()->format('d.m.Y'),
+            self::VAR_COURSE_TITLE => $event->getCourse() ? $event->getCourse()->title : null,
         ];
     }
 
