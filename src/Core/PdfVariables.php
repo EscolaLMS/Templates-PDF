fa48331362743108fc7189c72e5641f17a128d94
Witold Wiśniewski <58150098+daVitekPL@users.noreply.github.com>
2023-03-02 13:40:36 +0100

    Added product title variable (#25)
    * Added product title variable
    
    * Update composer.json
    
    * Fix tests
    
    * Update composer.json
    
    * Update composer.json

--- src/Core/PdfVariables.php
+++ src/Core/PdfVariables.php
@@ -11,7 +11,6 @@ abstract class PdfVariables extends AbstractTemplateVariableClass implements Tem
 {
     const VAR_APP_NAME       = '@VarAppName';
     const VAR_TODAY = '@VarToday';
-    const VAR_COURSE_TITLE = '@VarCourseTitle';
 
     public static function mockedVariables(?User $user = null): array
     {
@@ -19,7 +18,6 @@ abstract class PdfVariables extends AbstractTemplateVariableClass implements Tem
         return [
             self::VAR_APP_NAME => config('app.name'),
             self::VAR_TODAY => today()->format('d.m.Y'),
-            self::VAR_COURSE_TITLE => $faker->title,
         ];
     }
 
@@ -28,7 +26,6 @@ abstract class PdfVariables extends AbstractTemplateVariableClass implements Tem
         return [
             self::VAR_APP_NAME => config('app.name'),
             self::VAR_TODAY => today()->format('d.m.Y'),
-            self::VAR_COURSE_TITLE => $event->getCourse() ? $event->getCourse()->title : null,
         ];
     }
 
