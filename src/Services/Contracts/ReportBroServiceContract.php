c45697031ca3234296055418c886b0aea569dc30
Mateusz Wojczal <mateusz@wojczal.com>
2023-05-15 16:58:08 +0200

    report bro

--- src/Services/Contracts/ReportBroServiceContract.php
+++ src/Services/Contracts/ReportBroServiceContract.php
@@ -9,9 +9,5 @@ interface ReportBroServiceContract
     public function getKeyFromPayload(string $payload): string;
     public function getFilepathFromKey(string $key): string;
     public function passAll(Request $request): string;
-<<<<<<< HEAD
     public function generateFileFromRecord(int $id, bool $testData): string;
-=======
-    public function generateFileFromRecord(int $id): string;
->>>>>>> 491a36e0830401532643dbac02a778f5f5d37d07
 }
